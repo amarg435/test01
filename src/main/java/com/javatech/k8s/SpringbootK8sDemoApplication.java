5d9089e70b8ec1673ed2b77890c11d76e6768e9d
amarg435 <71606560+amarg435@users.noreply.github.com>
2023-04-03 21:08:41 +0530

    Update SpringbootK8sDemoApplication.java

--- src/main/java/com/javatech/k8s/SpringbootK8sDemoApplication.java
+++ src/main/java/com/javatech/k8s/SpringbootK8sDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringbootK8sDemoApplication {
 
 	@GetMapping("/message")
 	public String displayMessage(){
-		return "Congratulation you successfully deployed your application to kubernetes_ Version THREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE !!";
+		return "Congratulation you successfully deployed your application to kubernetes_ Version FOURRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR!!";
 	}
 
 	public static void main(String[] args) {
